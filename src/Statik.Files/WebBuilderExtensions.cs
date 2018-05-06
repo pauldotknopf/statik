e0c1746a0ddbf1c7ac78d15b566208a6638c006e
Paul Knopf <pauldotknopf@gmail.com>
2018-05-05 21:42:55 -0400

    Added support for static files.

---
+++ src/Statik.Files/WebBuilderExtensions.cs
@@ -0,0 +1,80 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Statik.Web;
+
+namespace Statik.Files
+{
+    public static class WebBuilderExtensions
+    {
+        public static void RegisterFiles(this IWebBuilder webBuilder, IFileProvider fileProvider)
+        {
+            RegisterFiles(webBuilder, "/", fileProvider);
+        }
+
+        public static void RegisterFiles(this IWebBuilder webBuilder, PathString prefix, IFileProvider fileProvider)
+        {
+            var contents = fileProvider.GetDirectoryContents("/");
+            if(contents == null || !contents.Exists) return;
+
+            foreach(var file in contents)
+            {
+                webBuilder.RegisterFileInfo(fileProvider, prefix, "/", file);
+            }
+        }
+
+        private static void RegisterFileInfo(this IWebBuilder webBuilder, IFileProvider fileProvider, PathString prefix,
+            string basePath, IFileInfo fileInfo)
+        {
+            if (fileInfo.IsDirectory)
+            {
+                var content = fileProvider.GetDirectoryContents(fileInfo.Name);
+
+                if (content == null || !content.Exists)
+                {
+                    return;
+                }
+
+                foreach (var child in content)
+                {
+                    webBuilder.RegisterFileInfo(fileProvider, prefix, Path.Combine(basePath, fileInfo.Name), child);
+                }
+            }
+            else
+            {
+                var path = new PathString().Add(prefix)
+                    .Add(basePath)
+                    .Add("/" + fileInfo.Name);
+
+                webBuilder.Register(path.Value, async context =>
+                {
+                    var env = context.RequestServices.GetRequiredService<IHostingEnvironment>();
+
+                    var options = Options.Create(new StaticFileOptions());
+                    options.Value.FileProvider = fileProvider;
+
+                    var loggerFactory = context.RequestServices.GetRequiredService<ILoggerFactory>();
+                    var middleware = new StaticFileMiddleware(_ => Task.CompletedTask, env, options, loggerFactory);
+
+                    var oldPath = context.Request.Path;
+                    try
+                    {
+                        context.Request.Path = Path.Combine(basePath, fileInfo.Name);
+                        await middleware.Invoke(context);
+                    }
+                    finally
+                    {
+                        context.Request.Path = oldPath;
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ test/Statik.Files.Tests/FileProviderTests.cs
@@ -0,0 +1,122 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.FileProviders;
+using Statik.Hosting;
+using Statik.Hosting.Impl;
+using Statik.Tests;
+using Statik.Web;
+using Statik.Web.Impl;
+using Statik.Files;
+using Xunit;
+
+namespace Statik.Files.Tests
+{
+    public class FileProviderTests
+    {
+        readonly IWebBuilder _webBuilder;
+
+        public FileProviderTests()
+        {
+            _webBuilder = new WebBuilder(new HostBuilder());
+        }
+        
+        [Fact]
+        public async Task Can_register_files()
+        {
+            using (var testDirectory = new WorkingDirectorySession())
+            {
+                File.WriteAllText(Path.Combine(testDirectory.Directory, "test.txt"), "test");
+                Directory.CreateDirectory(Path.Combine(testDirectory.Directory, "nested"));
+                File.WriteAllText(Path.Combine(testDirectory.Directory, "nested", "test2.txt"), "test2");
+                
+                _webBuilder.RegisterFiles(new PhysicalFileProvider(testDirectory.Directory));
+                
+                using(var host = _webBuilder.BuildVirtualHost())
+                {
+                    using(var client = host.CreateClient())
+                    {
+                        var responseMessage = await client.GetAsync("/test.txt");
+                        responseMessage.EnsureSuccessStatusCode();
+                        var response = await responseMessage.Content.ReadAsStringAsync();
+
+                        Assert.Equal("test", response);
+                        
+                        responseMessage = await client.GetAsync("/nested/test2.txt");
+                        responseMessage.EnsureSuccessStatusCode();
+                        response = await responseMessage.Content.ReadAsStringAsync();
+
+                        Assert.Equal("test2", response);
+                    }
+                }
+                
+                using(var host = _webBuilder.BuildVirtualHost("/appbase"))
+                {
+                    using(var client = host.CreateClient())
+                    {
+                        var responseMessage = await client.GetAsync("/test.txt");
+                        responseMessage.EnsureSuccessStatusCode();
+                        var response = await responseMessage.Content.ReadAsStringAsync();
+
+                        Assert.Equal("test", response);
+                        
+                        responseMessage = await client.GetAsync("/nested/test2.txt");
+                        responseMessage.EnsureSuccessStatusCode();
+                        response = await responseMessage.Content.ReadAsStringAsync();
+
+                        Assert.Equal("test2", response);
+                    }
+                }
+            }
+        }
+        
+        [Fact]
+        public async Task Can_register_files_at_path()
+        {
+            using (var testDirectory = new WorkingDirectorySession())
+            {
+                File.WriteAllText(Path.Combine(testDirectory.Directory, "test.txt"), "test");
+                Directory.CreateDirectory(Path.Combine(testDirectory.Directory, "nested"));
+                File.WriteAllText(Path.Combine(testDirectory.Directory, "nested", "test2.txt"), "test2");
+                
+                _webBuilder.RegisterFiles("/prefix", new PhysicalFileProvider(testDirectory.Directory));
+                
+                using(var host = _webBuilder.BuildVirtualHost())
+                {
+                    using(var client = host.CreateClient())
+                    {
+                        var responseMessage = await client.GetAsync("/prefix/test.txt");
+                        responseMessage.EnsureSuccessStatusCode();
+                        var response = await responseMessage.Content.ReadAsStringAsync();
+
+                        Assert.Equal("test", response);
+                        
+                        responseMessage = await client.GetAsync("/prefix/nested/test2.txt");
+                        responseMessage.EnsureSuccessStatusCode();
+                        response = await responseMessage.Content.ReadAsStringAsync();
+
+                        Assert.Equal("test2", response);
+                    }
+                }
+                
+                using(var host = _webBuilder.BuildVirtualHost("/appbase"))
+                {
+                    using(var client = host.CreateClient())
+                    {
+                        var responseMessage = await client.GetAsync("/prefix/test.txt");
+                        responseMessage.EnsureSuccessStatusCode();
+                        var response = await responseMessage.Content.ReadAsStringAsync();
+
+                        Assert.Equal("test", response);
+                        
+                        responseMessage = await client.GetAsync("/prefix/nested/test2.txt");
+                        responseMessage.EnsureSuccessStatusCode();
+                        response = await responseMessage.Content.ReadAsStringAsync();
+
+                        Assert.Equal("test2", response);
+                    }
+                }
+            }
+        }
+    }
+}

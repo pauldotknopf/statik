11163ec851dde32e3ad0aae57bb5e75018054857
Paul Knopf <pauldotknopf@gmail.com>
2018-07-01 13:35:14 -0400

    Added more unit tests.

--- src/Statik.Pages/Impl/PageDirectoryLoader.cs
+++ src/Statik.Pages/Impl/PageDirectoryLoader.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.FileSystemGlobbing;
-using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
 
 namespace Statik.Pages.Impl
 {
@@ -11,28 +10,47 @@ namespace Statik.Pages.Impl
     {
         public MenuItem<IFileInfo> LoadFiles(IFileProvider fileProvider, Matcher pageMatcher, Matcher indexMatcher)
         {
+            return LoadDirectory(fileProvider, "/", null, pageMatcher, indexMatcher);
+        }
+
+        private MenuItem<IFileInfo> LoadDirectory(IFileProvider fileProvider, string basePath, IFileInfo parentDirectory, Matcher pageMatcher, Matcher indexMatcher)
+        {
             MenuItem<IFileInfo> root = null;
-            var files = fileProvider.GetDirectoryContents("/")
+            var files = fileProvider.GetDirectoryContents(basePath)
                 .ToList();
 
             var children = new List<MenuItem<IFileInfo>>();
 
+            // Load all the files
             foreach (var file in files.Where(x => !x.IsDirectory))
             {
                 if (indexMatcher.Match(file.Name).HasMatches)
                 {
                     // This is the index page.
                     root = new MenuItem<IFileInfo>(file);
-                } else if (pageMatcher.Match(file.Name).HasMatches)
+                }
+                else if (pageMatcher.Match(file.Name).HasMatches)
                 {
                     children.Add(new MenuItem<IFileInfo>(file));
                 }
             }
             
             if(root == null)
-                root = new MenuItem<IFileInfo>(null);
+                root = new MenuItem<IFileInfo>(parentDirectory);
             
             root.Children.AddRange(children);
+            
+            // Load all the child directories
+            foreach (var directory in files.Where(x => x.IsDirectory))
+            {
+                var path = new PathString().Add(basePath)
+                    .Add("/" + directory.Name);
+                var menuItem = LoadDirectory(fileProvider, path, directory, pageMatcher, indexMatcher);
+                if (menuItem != null)
+                {
+                    root.Children.Add(menuItem);
+                }
+            }
 
             return root;
         }
--- test/Statik.Pages.Tests/PageDirectoryLoaderTests.cs
+++ test/Statik.Pages.Tests/PageDirectoryLoaderTests.cs
@@ -1,13 +1,18 @@
-﻿using Dazinator.AspNet.Extensions.FileProviders;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace Statik.Pages.Tests
 {
-    public class PageDirectoryLoaderTests
+    public class PageDirectoryLoaderTests : IDisposable
     {
-        IPageDirectoryLoader _pageDirectoryLoader;
+        readonly IPageDirectoryLoader _pageDirectoryLoader;
+        readonly string _directory;
         
         public PageDirectoryLoaderTests()
         {
@@ -15,20 +20,73 @@ namespace Statik.Pages.Tests
             Statik.RegisterServices(services);
             StatikPages.RegisterServices(services);
             _pageDirectoryLoader = services.BuildServiceProvider().GetRequiredService<IPageDirectoryLoader>();
+            _directory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Guid.NewGuid().ToString());
+            Directory.CreateDirectory(_directory);
         }
         
         [Fact]
         public void Can_load_pages()
         {
-            var fileProvider = new InMemoryFileProvider();
-            fileProvider.Directory.AddFile("/", new StringFileInfo("content", "index.md"));
-            fileProvider.Directory.AddFile("/", new StringFileInfo("content", "file.md"));
+            File.WriteAllText(Path.Combine(_directory, "index.md"), "content");
+            File.WriteAllText(Path.Combine(_directory, "file.md"), "content");
+            File.WriteAllText(Path.Combine(_directory, "test.txt"), "content");
             
-            var menuItem = _pageDirectoryLoader.LoadFiles(fileProvider, "*.md", "index.md");
+            var menuItem = _pageDirectoryLoader.LoadFiles(new PhysicalFileProvider(_directory),
+                "*.md",
+                "index.md");
 
             menuItem.Data.Name.Should().Be("index.md");
             menuItem.Children.Count.Should().Be(1);
             menuItem.Children[0].Data.Name.Should().Be("file.md");
+        }
+
+        [Fact]
+        public void Can_load_nested_pages()
+        {
+            File.WriteAllText(Path.Combine(_directory, "index.md"), "content");
+            File.WriteAllText(Path.Combine(_directory, "file.md"), "content");
+            Directory.CreateDirectory(Path.Combine(_directory, "test"));
+            File.WriteAllText(Path.Combine(_directory, "test", "index.md"), "content");
+            File.WriteAllText(Path.Combine(_directory, "test", "file2.md"), "content");
+            
+            var menuItem = _pageDirectoryLoader.LoadFiles(new PhysicalFileProvider(_directory),
+                "*.md",
+                "index.md");
+
+            menuItem.Data.Name.Should().Be("index.md");
+            menuItem.Children.Count.Should().Be(2);
+            menuItem.Children[0].Data.Name.Should().Be("file.md");
+            menuItem.Children[0].Data.IsDirectory.Should().BeFalse();
+            menuItem.Children[1].Data.Name.Should().Be("index.md");
+            menuItem.Children[1].Children.Count.Should().Be(1);
+            menuItem.Children[1].Children[0].Data.Name.Should().Be("file2.md");
+            menuItem.Children[1].Children[0].Data.IsDirectory.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Can_create_empty_node_if_intermediate_child_has_no_page()
+        {
+            File.WriteAllText(Path.Combine(_directory, "index.md"), "content");
+            Directory.CreateDirectory(Path.Combine(_directory, "child", "grandchild"));
+            File.WriteAllText(Path.Combine(_directory, "child", "grandchild", "index.md"), "content");
+            
+            var menuItem = _pageDirectoryLoader.LoadFiles(new PhysicalFileProvider(_directory),
+                "*.md",
+                "index.md");
+
+            Debug.WriteLine(JsonConvert.SerializeObject(menuItem, Formatting.Indented));
+            
+            menuItem.Data.Name.Should().Be("index.md");
+            menuItem.Children.Count.Should().Be(1);
+            menuItem.Children[0].Data.IsDirectory.Should().Be(true);
+            menuItem.Children[0].Data.Name.Should().Be("child");
+            menuItem.Children[0].Children.Count.Should().Be(1);
+            menuItem.Children[0].Children[0].Data.Name.Should().Be("index.md");
+        }
+        
+        public void Dispose()
+        {
+            Directory.Delete(_directory, true);
         }
     }
 }
